1507083f9ec41be99fa6841110f6e6157e55c257
Ilya Kriel <ilyakriel@gmail.com>
2023-08-09 09:43:28 +1200

    Added get card event and get card item
    Not working yet

--- ZIMP_game/card_deck.py
+++ ZIMP_game/card_deck.py
@@ -1,4 +1,5 @@
 from card import Card
+import random
 
 
 class CardDeck:
@@ -7,6 +8,7 @@ class CardDeck:
         self.doom_clock = 9
         self.card_deck = []
         self.deck_counter = 0
+        self.discarded_cards = []
 
     def add_card_to_card_deck(self, image_number, event_9pm, event_10pm, event_11pm):
         card = Card(image_number, event_9pm, event_10pm, event_11pm)
@@ -14,8 +16,30 @@ class CardDeck:
         self.deck_counter = len(self.card_deck)
 
     # need to draw a card (a random card)
-    def get_card_event(self, target_event_time):
-        return next((event for event in self.card_deck if event.get_event(target_event_time) == target_event_time))
+    def get_card_event(self):
+        # while the card is not in the discarded_cards return the card event == doom_counter
+        # select a random card from the deck
+        # get a random number 0-8
+
+        while True:
+            random_number = random.randint(0, 8)
+            random_card = self.card_deck[random_number]
+            discarded_card = next((card for card in self.discarded_cards if card == random_card), None)
+
+            if discarded_card != random_card:
+                # add the card to the discarded_cards
+                self.discarded_cards.append(random_card)
+                # reduce the number of cards available to draw from
+                self.reduce_deck_counter()
+                return random_card.get_event(self.doom_clock)
+
+    def get_card_item(self, discard_card):
+        # if discard a card to get an item -> add card to the discarded_cards & reduce_deck_counter
+        if discard_card:
+
+        # else get random card not in discarded_cards to return the card item
+        # & then add card to the discarded_cards & reduce_deck_counter
+        pass
 
     # reduce the deck_counter on player action
     def reduce_deck_counter(self):
