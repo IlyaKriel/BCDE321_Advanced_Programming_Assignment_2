bd6738c4291b470ad563ed149b53109fc03fe5b7
Ilya <ilya@gmail.com>
2023-08-20 11:41:16 +1200

    Added sudo code

--- ZIMP_game/models/game.py
+++ ZIMP_game/models/game.py
@@ -1,5 +1,27 @@
-from card import Card
+class Game:
 
-card = Card("1", "Don't pee yourself", "Item", "6 zombies")
-card.set_item("Chainsaw", 2, 3)
-card.get_item_image()
+    def __init__(self):
+        pass
+
+    # Set up the environment
+    # set up the indoor tile deck -> includes the evil temple
+    # if on kitchen or garden tile end turn -> gain health +1
+    # if no exit -> fight 3 zombies breaking through wall to create new exit of choice
+    # set up the outdoor tile deck -> includes the graveyard
+
+    # Set up the player
+    # set the player position
+    # set the player health
+    # set the player attack
+    # set the player item 1
+    # set the player item 2
+    # set the totem slot
+
+    # Set up the dev_cards
+    # add cards
+    # set card item
+    # set card event
+
+    # game progression -> set up the find/bury totem action
+    # player action -> cower for health but not before doorway event
+    # draw dev card when player enters new room or re-enters previously explored room
